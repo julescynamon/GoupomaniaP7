e29e277edfe9fdb72a36b1db33dacb8f4c86746f
cynamon jules <julescweb@mail.com>
2022-04-05 13:28:38 +0200

    mise en place de la partie login

---
+++ frontend/src/components/log/auth.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function auth() {
+    return (
+        <div>
+            <div className="connection-form">
+                <div className="form-container">
+                    <ul>
+                        <li>
+
+                        </li>
+                        <li>
+                            
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
--- frontend/src/pages/Profil.js
+++ frontend/src/pages/Profil.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Log from "../components/log/auth";
 
 export default function Profil() {
     return (
-		<div>
-			<h1>Bonjour profil</h1>
+		<div className='profil-page'>
+			<div className='log-container'>
+				<Log />
+				<div className='img-container'>
+					<img
+						src='./img/icon-left-font.svg'
+						alt='Groupomania icone'
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
